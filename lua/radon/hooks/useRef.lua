e9c5289cc53f8c36fa26a9f9919fe0fdeb9d7f6d
dromse <dromseproducer@gmail.com>
2023-11-01 01:19:15 +0200

    add Ref ending to useRef variable

--- lua/radon/hooks/useRef.lua
+++ lua/radon/hooks/useRef.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.generate = function(opts)
-	return 'const ' .. opts.name .. ' = useRef(' .. opts.value .. ')'
+	return 'const ' .. opts.name .. 'Ref = useRef(' .. opts.value .. ')'
 end
 
 return M
